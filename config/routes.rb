d6c9691183840a46a5f9e2def9462d89053214a0
NoahNovick <nnovickfaille1@gmail.com>
2020-04-02 16:19:45 -0400

    login feature

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,14 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show]
   end
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
