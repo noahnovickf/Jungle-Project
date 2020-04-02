16e92636d9c74b10c5a15b7c476e41c1499651d8
NoahNovick <nnovickfaille1@gmail.com>
2020-04-01 21:57:17 -0400

    admin count feature

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'].to_s, password: ENV['PASSWORD'].to_s
   def show
-
+    @products_count = Product.count
+    @categories_count = Category.count
   end
 end
