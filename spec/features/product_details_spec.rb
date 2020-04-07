8dfdcc4e12eed6e52dc2c75ab529d1d824962e4d
NoahNovick <nnovickfaille1@gmail.com>
2020-04-07 16:15:03 -0400

    testing product detail page is accessible from home page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+  name:  Faker::Hipster.sentence(3),
+  description: Faker::Hipster.paragraph(4),
+  image: open_asset('apparel1.jpg'),
+  quantity: 10,
+  price: 64.99
+)
+    end
+  end
+  
+  scenario "They see product details" do
+    visit root_path
+    first('.product').click_link('Details')
+    expect(page).to have_css('.products-show')
+  end
+
+end
